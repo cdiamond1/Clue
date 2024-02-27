b89c22d849203b1fe9be8e35e274cd892f2bf167
cdupras <charlie@duprasville.com>
2024-02-27 14:00:08 -0700

    calcTargets written, translated from lecture pseudocode

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -23,11 +23,29 @@ public class TestBoard {
 	final static int ROWS = 4;
 	
 	 public TestBoard() {
-		 
+
 	 }
 	 
-	 public void calcTargets(TestBoardCell startCell, int pathlength) {
-		 
+	 public void calcTargets(TestBoardCell startCell, int pathLength) {
+		 // iterate through every cell of startCell adjacent list
+		 for (TestBoardCell cell: startCell.getAdjList()) {
+			 // if already visited, skip this cell
+			 if (visited.contains(cell)) continue;
+			 
+			 // add cell to visited list
+			 visited.add(cell);
+			 
+			 // add cell to targets if out of moves
+			 if (pathLength == 1) {
+				 targets.add(cell);
+			 }
+			 // recursive call if moves left
+			 else {
+				 this.calcTargets(cell, pathLength - 1);
+			 }
+			 
+			 visited.remove(cell);
+		 }
 	 }
 	 	
 	// getCell: returns cell given row+column. If cell doesn't exist, returns null
