ce17be0b2e52ddf703fc766371464b48c7243afc
cdupras <charlie@duprasville.com>
2024-03-04 09:34:54 -0700

    refactored loadLayoutConfig, FileInitTests.java now passes completely

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -3,6 +3,7 @@ package clueGame;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -37,7 +38,7 @@ public class Board {
 	private String layoutConfigFile;
 	private String setupConfigFile;
 	private String initial;
-	private Map<Character, Room> roomMap;
+	private Map<Character, Room> roomMap = new HashMap<Character, Room>();
 	private static Board theInsrance;
 	private Scanner in;
 	private File csv;
@@ -56,12 +57,10 @@ public class Board {
 	protected Room Walkway = new Room("Walkway");
 	protected Room Unused = new Room();
 
-	
 	// Temp Vars
 	private BoardCell temp;
 
 	/*
-<<<<<<< HEAD
 	 * variable and methods used for singleton pattern
 	 */
 	private static Board theInstance = new Board();
@@ -81,8 +80,8 @@ public class Board {
 	 */
 	public void initialize() {
 		try {
-			loadLayoutConfig();
 			loadSetupConfig();
+			loadLayoutConfig();
 		} catch (BadConfigFormatException e) {
 			e.printStackTrace();
 		}
@@ -158,36 +157,44 @@ public class Board {
 	// getCell: returns cell given row+column. If cell doesn't exist, returns null
 	public BoardCell getCell(int col, int row) {
 		// iterate through each cell in targets list
-		for (BoardCell cell : boardCells) {
-			if (cell.getColumn() == col && cell.getRow() == row) {
-				return cell;
-			}
-		}
-		// if no cell returned, return null (temp for now)
-		BoardCell temp = new BoardCell(row, col, "X");
-		return temp;
+//		for (BoardCell cell : boardCells) {
+//			if (cell.getColumn() == col && cell.getRow() == row) {
+//				return cell;
+//			}
+//		}
+		return grid[row][col];
 	}
 
 	public Set<BoardCell> getTargets() {
 		return targets;
 	}
 
+	// Loads setup file, populating roomMap
 	public void loadSetupConfig() throws BadConfigFormatException {
 		String line;
 		String[] lineSplit = new String[3];
-		String[] symbols = new String[11];
+		
+		// read file
 		try {
 			in = new Scanner(txt);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 		while(in.hasNextLine()) {
 			line = in.nextLine();
+			
+			// skip comments
 			if(line.charAt(0) == '/') {
 				continue;
 			}
+			
+			// split at comma
 			lineSplit = line.split(", ");
-			if(lineSplit[0].equals("Room") || lineSplit[0].equals("Space")) {
+			if((lineSplit[0].equals("Room") || lineSplit[0].equals("Space")) && lineSplit.length == 3) {
+				// create room object
+				Room temp = new Room(lineSplit[1]);
+				roomMap.put(lineSplit[2].charAt(0), temp);
 				
 			} else {
 				throw new BadConfigFormatException("Format error in setup file: " + lineSplit[0]);
@@ -195,159 +202,7 @@ public class Board {
 		}
 	}
 
-	/*
-	public void loadLayoutConfig() throws BadConfigFormatException {
-		// Initializes and fills the grid with cells
-		grid = new BoardCell[boardRows][boardCols];
-		try {
-			in = new Scanner(csv);
-			in.useDelimiter(",");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		for (int rows = 0; rows < boardRows; rows++) {
-			for (int cols = 0; cols < boardCols - 1; cols++) {
-				if (in.hasNext()) {
-					initial = in.next();
-					if(initial.equals("") || initial.equals(" ") || initial.equals(null)) {
-						throw new BadConfigFormatException();
-					}
-					temp = new BoardCell(rows, cols, initial);
-					
-					// Reading first char
-					switch (initial.charAt(0)) {
-					case 'E':
-						temp.setRoomLoc(Engine_Room);
-						temp.setRoom(true);
-						break;
-
-					case 'N':
-						temp.setRoomLoc(Navigation);
-						temp.setRoom(true);
-						break;
-
-					case 'S':
-						temp.setRoomLoc(Shields);
-						temp.setRoom(true);
-						break;
-
-					case 'L':
-						temp.setRoomLoc(Life_Support);
-						temp.setRoom(true);
-						break;
-
-					case 'P':
-						temp.setRoomLoc(Weapons);
-						temp.setRoom(true);
-						break;
-
-					case 'M':
-						temp.setRoomLoc(Medical_Bay);
-						temp.setRoom(true);
-						break;
-
-					case 'C':
-						temp.setRoomLoc(Cargo);
-						temp.setRoom(true);
-						break;
-
-					case 'H':
-						temp.setRoomLoc(Hydroponics);
-						temp.setRoom(true);
-						break;
-
-					case 'R':
-						temp.setRoomLoc(Reactor);
-						temp.setRoom(true);
-						break;
-
-					case 'W':
-						temp.setRoomLoc(Walkway);
-						temp.setRoom(true);
-						break;
-						
-					default:
-						if(initial.charAt(0) != 'X') {
-							throw new BadConfigFormatException("Bad room symbol: " + initial.charAt(0));
-						}
-						temp.setRoomLoc(Unused);
-						temp.setRoom(false);
-						temp.setOccupied(true);
-						break;
-					}
-
-					// Reading second char if there is one
-					if (initial.length() > 1) {
-						switch (initial.charAt(1)) {
-						case '<':
-							temp.setDoorway(true);
-							temp.setDoorDirection(DoorDirection.LEFT);
-							break;
-						case '^':
-							temp.setDoorway(true);
-							temp.setDoorDirection(DoorDirection.UP);
-							break;
-						case '>':
-							temp.setDoorway(true);
-							temp.setDoorDirection(DoorDirection.RIGHT);
-							break;
-						case 'v':
-							temp.setDoorway(true);
-							temp.setDoorDirection(DoorDirection.DOWN);
-							break;
-						case '*':
-							temp.setRoomCenter(true);
-							temp.getRoom().setCenterCell(temp);
-							break;
-						case '#':
-							temp.setRoomLabel(true);
-							temp.getRoom().setLabelCell(temp);
-							break;
-						default:
-							temp.setSecretPassage(initial.charAt(1));
-							break;
-						}
-					}
-					grid[rows][cols] = temp;
-					boardCells.add(temp);
-				}
-			}
-		}
-
-		// Generates each cells adjacency lists (Attempted to do in a switch/case but
-		// couldn't get it to compile)
-		for (int y = 0; y < boardRows; y++) {
-			for (int x = 0; x < boardCols; x++) {
-				test = getCell(x, y);
-				temp = getCell(x + 1, y); // Tests adjacent cell to the right
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
-					}
-				}
-				temp = getCell(x - 1, y); // Tests adjacent cell to the left
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
-					}
-				}
-				temp = getCell(x, y + 1); // Tests adjacent cell above
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
-					}
-				}
-				temp = getCell(x, y - 1); // Tests adjacent cell below
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
-					}
-				}
-			}
-		}
-	}
-	*/
-
+	// loads game board layout
 	public void loadLayoutConfig() throws BadConfigFormatException {
 		ArrayList<String[]> dataList = new ArrayList<>();
 		
@@ -357,71 +212,27 @@ public class Board {
 		// create grid with proper size
 		grid = new BoardCell[boardRows][boardCols];
 		
+		// iterate through every cell in game board
 		for (int row = 0; row < boardRows; row++) {
 			for (int col = 0; col < boardCols; col++) {
 				initial = dataList.get(row)[col];
 				temp = new BoardCell(row, col, initial);
 				
-				// Reading first char
-				switch (initial.charAt(0)) {
-				case 'E':
-					temp.setRoomLoc(Engine_Room);
-					temp.setRoom(true);
-					break;
-
-				case 'N':
-					temp.setRoomLoc(Navigation);
-					temp.setRoom(true);
-					break;
-
-				case 'S':
-					temp.setRoomLoc(Shields);
-					temp.setRoom(true);
-					break;
-
-				case 'L':
-					temp.setRoomLoc(Life_Support);
-					temp.setRoom(true);
-					break;
-
-				case 'P':
-					temp.setRoomLoc(Weapons);
-					temp.setRoom(true);
-					break;
-
-				case 'M':
-					temp.setRoomLoc(Medical_Bay);
-					temp.setRoom(true);
-					break;
-
-				case 'C':
-					temp.setRoomLoc(Cargo);
-					temp.setRoom(true);
-					break;
-
-				case 'H':
-					temp.setRoomLoc(Hydroponics);
-					temp.setRoom(true);
-					break;
-
-				case 'R':
-					temp.setRoomLoc(Reactor);
-					temp.setRoom(true);
-					break;
-
-				case 'W':
-					temp.setRoomLoc(Walkway);
-					temp.setRoom(true);
-					break;
-					
-				default:
-					if(initial.charAt(0) != 'X') {
-						throw new BadConfigFormatException("Bad room symbol: " + initial.charAt(0));
-					}
+				if (initial.charAt(0) == 'X') {
 					temp.setRoomLoc(Unused);
 					temp.setRoom(false);
 					temp.setOccupied(true);
-					break;
+				}
+				else if (initial.charAt(0) == 'W') {
+					temp.setRoomLoc(Walkway);
+					temp.setRoom(true);
+				}
+				else if (roomMap.get(initial.charAt(0)) != null) {
+					temp.setRoomLoc(roomMap.get(initial.charAt(0)));
+					temp.setRoom(true);
+				}
+				else {
+					throw new BadConfigFormatException("Bad room symbol: " + initial.charAt(0));
 				}
 
 				// Reading second char if there is one
@@ -461,33 +272,44 @@ public class Board {
 			}
 		}
 
-		// Generates each cells adjacency lists (Attempted to do in a switch/case but
-		// couldn't get it to compile)
+		// Generates each cells adjacency lists
 		for (int y = 0; y < boardRows; y++) {
 			for (int x = 0; x < boardCols; x++) {
 				test = getCell(x, y);
-				temp = getCell(x + 1, y); // Tests adjacent cell to the right
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
+				
+				if (x + 1 < boardCols) {
+					temp = getCell(x + 1, y); // Tests adjacent cell to the right
+					if (temp != null) {
+						if (temp.isOccupied() == false && temp.isRoom() == false) {
+							test.addAdjacency(temp);
+						}
 					}
 				}
-				temp = getCell(x - 1, y); // Tests adjacent cell to the left
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
+				
+				if (x - 1 >= 0) {
+					temp = getCell(x - 1, y); // Tests adjacent cell to the left
+					if (temp != null) {
+						if (temp.isOccupied() == false && temp.isRoom() == false) {
+							test.addAdjacency(temp);
+						}
 					}
 				}
-				temp = getCell(x, y + 1); // Tests adjacent cell above
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
+				
+				if (y + 1 < boardRows) {
+					temp = getCell(x, y + 1); // Tests adjacent cell above
+					if (temp != null) {
+						if (temp.isOccupied() == false && temp.isRoom() == false) {
+							test.addAdjacency(temp);
+						}
 					}
 				}
-				temp = getCell(x, y - 1); // Tests adjacent cell below
-				if (temp != null) {
-					if (temp.isOccupied() == false && temp.isRoom() == false) {
-						test.addAdjacency(temp);
+				
+				if (y - 1 >= 0) {
+					temp = getCell(x, y - 1); // Tests adjacent cell below
+					if (temp != null) {
+						if (temp.isOccupied() == false && temp.isRoom() == false) {
+							test.addAdjacency(temp);
+						}
 					}
 				}
 			}
@@ -498,7 +320,6 @@ public class Board {
 	public void setConfigFiles(String string, String string2) {
 		csv = new File(string);
 		txt = new File(string2);
-
 	}
 
 	public Room getRoom(char c) {
