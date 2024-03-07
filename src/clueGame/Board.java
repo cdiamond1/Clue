59ba12e3d2727849df7f5ab6d627a303f931dd25
cdiamond1 <cdiamond1@mines.edu>
2024-03-06 17:27:10 -0700

    Added changes from code review day

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -33,30 +33,12 @@ public class Board {
 	private static int boardRows = 30;
 
 	// C14A-2 additional variables
-	private String layoutConfigFile;
-	private String setupConfigFile;
 	private String initial;
 	private Map<Character, Room> roomMap = new HashMap<Character, Room>();
 	private static Board theInsrance;
-	private Scanner in;
+
 	private File csv;
 	private File txt;
-
-	// Rooms
-	protected Room Engine_Room = new Room("Engine Room");
-	protected Room Navigation = new Room("Navigation");
-	protected Room Shields = new Room("Shields");
-	protected Room Life_Support = new Room("Life Support");
-	protected Room Weapons = new Room("Weapons");
-	protected Room Medical_Bay = new Room("Medical Bay");
-	protected Room Cargo = new Room("Cargo");
-	protected Room Hydroponics = new Room("Hydroponics");
-	protected Room Reactor = new Room("Reactor");
-	protected Room Walkway = new Room("Walkway");
-	protected Room Unused = new Room();
-
-	// Temp Vars
-	private BoardCell temp;
 
 	/*
 	 * variable and methods used for singleton pattern
@@ -84,119 +66,119 @@ public class Board {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void setConfigFiles(String string, String string2) {
 		csv = new File(string);
 		txt = new File(string2);
 	}
-	
+
 	// readData gets passed a file name, and returns a 2D array of the
 	// information in the file
-     public ArrayList<String[]> readData(File file) throws BadConfigFormatException {
-    	 ArrayList<String[]> dataList = new ArrayList<>();
-    	 
-    	 // read file
-    	 try {
-    		 Scanner scanner = new Scanner(file);
-    		 
-    		 boardRows = 0;
-    		 String currLine;
-    		 // get lines from file one at a time - split each line at comma
-    		 while(scanner.hasNextLine()) {
-    			 // add all file data to string
-    			 currLine = scanner.nextLine();
-    			 dataList.add(currLine.split(","));
-    			 
-    			 // check configuration
-    			 for (String item: dataList.get(boardRows)) {
-    				 if (item.equals("") || item.equals(" ") || item.equals(null)) {
-    					 throw new BadConfigFormatException();
-    				 }
-    			 }
-    			 
-    			 boardRows++;
-    		 }
-    		 
-    		 boardCols = dataList.get(0).length;
-    		 
-    		 scanner.close();
-    	 }
-    	 catch (FileNotFoundException e) {
-    		 e.printStackTrace();
-    	 }
-    	 catch (BadConfigFormatException e) {
+	public ArrayList<String[]> readData(File file) throws BadConfigFormatException {
+		ArrayList<String[]> dataList = new ArrayList<>();
+
+		// read file
+		try {
+			Scanner scanner = new Scanner(file);
+
+			boardRows = 0;
+			String currLine;
+			// get lines from file one at a time - split each line at comma
+			while (scanner.hasNextLine()) {
+				// add all file data to string
+				currLine = scanner.nextLine();
+				dataList.add(currLine.split(","));
+
+				// check configuration
+				for (String item : dataList.get(boardRows)) {
+					if (item.equals("") || item.equals(" ") || item.equals(null)) {
+						throw new BadConfigFormatException();
+					}
+				}
+
+				boardRows++;
+			}
+
+			boardCols = dataList.get(0).length;
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (BadConfigFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	 
-    	 return dataList;
-    }
-
-	
+		return dataList;
+	}
 
 	// Loads setup file, populating roomMap
 	public void loadSetupConfig() throws BadConfigFormatException {
+		Scanner in = null;
 		String line;
 		String[] lineSplit = new String[3];
-		
+
 		// read file
 		try {
+			// move global variables into methods if possible
 			in = new Scanner(txt);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		while(in.hasNextLine()) {
+
+		while (in.hasNextLine()) {
 			line = in.nextLine();
-			
+
 			// skip comments
-			if(line.charAt(0) == '/') {
+			if (line.charAt(0) == '/') {
 				continue;
 			}
-			
+
 			// split at comma
 			lineSplit = line.split(", ");
-			if((lineSplit[0].equals("Room") || lineSplit[0].equals("Space")) && lineSplit.length == 3) {
+			if ((lineSplit[0].equals("Room") || lineSplit[0].equals("Space")) && lineSplit.length == 3) {
 				// create room object
 				Room temp = new Room(lineSplit[1]);
 				roomMap.put(lineSplit[2].charAt(0), temp);
-				
+
 			} else {
 				throw new BadConfigFormatException("Format error in setup file: " + lineSplit[0]);
 			}
 		}
+		in.close();
 	}
 
 	// loads game board layout
 	public void loadLayoutConfig() throws BadConfigFormatException {
 		ArrayList<String[]> dataList = new ArrayList<>();
-		
+		BoardCell temp; // Temp var
+
 		// read data from file
 		dataList = readData(csv);
-		
+
 		// create grid with proper size
 		grid = new BoardCell[boardRows][boardCols];
-		
+
 		// iterate through every cell in game board
 		for (int row = 0; row < boardRows; row++) {
 			for (int col = 0; col < boardCols; col++) {
 				initial = dataList.get(row)[col];
+				// make temp variables in the method they are used in - it generally bad to have
+				// global variables that can be moved into methods
 				temp = new BoardCell(row, col, initial);
-				
+
+				// make initial.charAt(0) a variable for readibility, can also use a switch
+				// statement
 				if (initial.charAt(0) == 'X') {
-					temp.setRoomLoc(Unused);
+					temp.setRoomLoc(roomMap.get('X'));
 					temp.setRoom(false);
 					temp.setOccupied(true);
-				}
-				else if (initial.charAt(0) == 'W') {
-					temp.setRoomLoc(Walkway);
+				} else if (initial.charAt(0) == 'W') {
+					temp.setRoomLoc(roomMap.get('W'));
 					temp.setRoom(true);
-				}
-				else if (roomMap.get(initial.charAt(0)) != null) {
+				} else if (roomMap.get(initial.charAt(0)) != null) {
 					temp.setRoomLoc(roomMap.get(initial.charAt(0)));
 					temp.setRoom(true);
-				}
-				else {
+				} else {
 					throw new BadConfigFormatException("Bad room symbol: " + initial.charAt(0));
 				}
 
@@ -232,55 +214,50 @@ public class Board {
 						break;
 					}
 				}
+
+				// rename temp to be more descriptive
 				grid[row][col] = temp;
 				boardCells.add(temp);
 			}
 		}
 
 		// Generates each cells adjacency lists
+		// consider moving this to it's own method for modularity (calcAdjacencyList)
 		for (int y = 0; y < boardRows; y++) {
 			for (int x = 0; x < boardCols; x++) {
 				test = getCell(y, x);
-				
+
 				if (x + 1 < boardCols) {
 					temp = getCell(y, x + 1); // Tests adjacent cell to the right
-					if (temp != null) {
-						if (temp.isOccupied() == false && temp.isRoom() == false) {
-							test.addAdjacency(temp);
-						}
+					if (temp != null && temp.isOccupied() == false && temp.isRoom() == false) {
+						test.addAdjacency(temp);
 					}
 				}
-				
+
 				if (x - 1 >= 0) {
 					temp = getCell(y, x - 1); // Tests adjacent cell to the left
-					if (temp != null) {
-						if (temp.isOccupied() == false && temp.isRoom() == false) {
-							test.addAdjacency(temp);
-						}
+					if (temp != null && temp.isOccupied() == false && temp.isRoom() == false) {
+						test.addAdjacency(temp);
 					}
 				}
-				
+
 				if (y + 1 < boardRows) {
 					temp = getCell(y + 1, x); // Tests adjacent cell above
-					if (temp != null) {
-						if (temp.isOccupied() == false && temp.isRoom() == false) {
-							test.addAdjacency(temp);
-						}
+					if (temp != null && temp.isOccupied() == false && temp.isRoom() == false) {
+						test.addAdjacency(temp);
 					}
 				}
-				
+
 				if (y - 1 >= 0) {
 					temp = getCell(y - 1, x); // Tests adjacent cell below
-					if (temp != null) {
-						if (temp.isOccupied() == false && temp.isRoom() == false) {
-							test.addAdjacency(temp);
-						}
+					if (temp != null && temp.isOccupied() == false && temp.isRoom() == false) {
+						test.addAdjacency(temp);
 					}
 				}
 			}
 		}
 	}
-	
+
 	public void calcTargets(BoardCell startCell, int pathLength) {
 		visited.add(startCell);
 
@@ -332,7 +309,7 @@ public class Board {
 	}
 
 	public Set<BoardCell> getAdjList(int i, int j) {
-		return getCell(i,j).getAdjList();
+		return getCell(i, j).getAdjList();
 	}
 
 }
