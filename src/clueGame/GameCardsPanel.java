ce2a33e30f693f57e67024843c2edf70ccabdf73
cdiamond1 <cdiamond1@mines.edu>
2024-04-04 16:22:38 -0600

    Game Card Panel works, but starts to overlap with 4-6 cards in one section

--- src/clueGame/GameCardsPanel.java
+++ src/clueGame/GameCardsPanel.java
@@ -1,12 +1,10 @@
 package clueGame;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,10 +22,179 @@ import javax.swing.border.TitledBorder;
  */
 
 public class GameCardsPanel extends JPanel {
-	
+	JPanel panel = new JPanel();
+	ArrayList<Card> hand = new ArrayList<Card>();
+	ArrayList<Card> seen = new ArrayList<Card>();
+	int cardCount;
+
 	// constructor
 	public GameCardsPanel() {
+		setLayout(new GridLayout(3, 0));
+		setBorder(new TitledBorder(new EtchedBorder(), "Known Cards"));
 		
+		addTestCards();
+		
+		add(panel);
+		panel = createPeoplePanel();
+		add(panel);
+		panel = createRoomPanel();
+		add(panel);
+		panel = createWeaponPanel();
+		add(panel);
+	}
+
+	public void addTestCards() {
+		hand.add(new Card("Professor Plum", CardType.PERSON));
+		hand.add(new Card("Miss Scarlet", CardType.PERSON));
+		hand.add(new Card("Wrench", CardType.WEAPON));
+		
+		/*
+		 * Add these to test overlapping
+		seen.add(new Card("Mrs. White", CardType.PERSON));
+		seen.add(new Card("Colonel Mustard", CardType.PERSON));
+		seen.add(new Card("Reverand Green", CardType.PERSON));
+		seen.add(new Card("Mrs. Peacock", CardType.PERSON));
+		seen.add(new Card("Colonel Mustard", CardType.PERSON));
+		seen.add(new Card("Mrs. White", CardType.PERSON));
+		
+		seen.add(new Card("Colonel Mustard", CardType.PERSON));
+		seen.add(new Card("Mrs. Peacock", CardType.PERSON));
+		*/
+		
+		seen.add(new Card("Reverand Green", CardType.PERSON));
+		seen.add(new Card("Kitchen", CardType.ROOM));
+		seen.add(new Card("Knife", CardType.WEAPON));
+	}
+	
+	public JPanel createPeoplePanel() {
+		cardCount = 0;
+		panel.setLayout(new GridLayout(2, 0));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "People"));
+		JPanel panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "In Hand:"));
+		//panel2.setLayout(new GridLayout(0, 1));
+		JLabel Card = new JLabel();
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		for (Card C : hand) {
+			if (C.getCardType() == CardType.PERSON) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		cardCount = 0;
+		panel.add(panel2);
+		panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "Seen:"));
+		//panel2.setLayout(new GridLayout(0,1));
+		Card = new JLabel();											// Tried JTextField and JPanel but JLabel seems to work the best
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));		// Maybe there's a better way, but JLabel overlaps the least atm
+		for (Card C : seen) {											// Assignment uses JTextFields I think
+			if (C.getCardType() == CardType.PERSON) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		panel2.revalidate();
+		panel.add(panel2);
+		return panel;
+	}
+
+	public JPanel createRoomPanel() {
+		cardCount = 0;
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 0));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Rooms"));
+		JPanel panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "In Hand:"));
+		JLabel Card = new JLabel();
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		for (Card C : hand) {
+			if (C.getCardType() == CardType.ROOM) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		cardCount = 0;
+		panel.add(panel2);
+		panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "Seen:"));
+		Card = new JLabel();
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		for (Card C : seen) {
+			if (C.getCardType() == CardType.ROOM) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		panel.add(panel2);
+		return panel;
+	}
+
+	public JPanel createWeaponPanel() {
+		cardCount = 0;
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(2, 0));
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Weapons"));
+		JPanel panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "In Hand:"));
+		JLabel Card = new JLabel();
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		for (Card C : hand) {
+			if (C.getCardType() == CardType.WEAPON) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		cardCount = 0;
+		panel.add(panel2);
+		panel2 = new JPanel();
+		panel2.setBorder(new TitledBorder(new EtchedBorder(), "Seen:"));
+		Card = new JLabel();
+		Card.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		for (Card C : seen) {
+			if (C.getCardType() == CardType.WEAPON) {
+				Card = new JLabel(C.getCardName());
+				cardCount++;
+				panel2.add(Card);
+				panel2.revalidate();
+			}
+		}
+		if (cardCount == 0) {
+			Card = new JLabel("None");
+			panel2.add(Card);
+		}
+		panel.add(panel2);
+		return panel;
 	}
 	
 	public static void main(String[] args) {
