7157a82780fc8139eb629805d13c84aad0e0cbaa
cdupras <charlie@duprasville.com>
2024-04-04 13:53:17 -0600

    gameCardsPanel.java initialized

---
+++ src/clueGame/GameCardsPanel.java
@@ -0,0 +1,37 @@
+package clueGame;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+/*
+ * GameCardsPanel class. Holds information and methods
+ * about the game cards panel on the right of the GUI.
+ * 
+ * Authors:
+ * Carson Diamond
+ * Charlie Dupras
+ */
+
+public class GameCardsPanel extends JPanel {
+	
+	// constructor
+	public GameCardsPanel() {
+		
+	}
+	
+	public static void main(String[] args) {
+		GameCardsPanel panel = new GameCardsPanel(); // create the panel
+		JFrame frame = new JFrame();
+	}
+}
