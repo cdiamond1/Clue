8eacf6802955c4a1948735b213fa74ff3846004a
cdiamond1 <cdiamond1@mines.edu>
2024-04-06 16:05:50 -0600

    Created intial file for Game Baord Panel for C23A

---
+++ src/clueGame/GameBoardPanel.java
@@ -0,0 +1,25 @@
+package clueGame;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameBoardPanel extends JPanel {
+	
+	public GameBoardPanel() {
+		JPanel test = new JPanel();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameBoardPanel panel = new GameBoardPanel(); // create the panel
+		JFrame frame = new JFrame();
+		
+		
+		frame.setContentPane(panel); // put the panel in the frame
+		frame.setSize(570, 570); // size the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true); // make it visible
+	}
+
+}
